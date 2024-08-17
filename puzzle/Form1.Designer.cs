3f714c05bb631193e0e8d9275e2cfdfa6e6210bc
EstebanPastrana <esteban.pastrana@ulatina.net>
2024-08-17 10:58:22 -0600

    Agregar archivos de proyecto.

---
+++ puzzle/Form1.Designer.cs
@@ -0,0 +1,256 @@
+﻿namespace puzzle
+{
+    partial class frmGame
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            gbxMain = new GroupBox();
+            btn1 = new Button();
+            btn2 = new Button();
+            btn3 = new Button();
+            btn4 = new Button();
+            btn5 = new Button();
+            btn6 = new Button();
+            btn7 = new Button();
+            btn8 = new Button();
+            btn9 = new Button();
+            btn10 = new Button();
+            btn11 = new Button();
+            btn12 = new Button();
+            btn13 = new Button();
+            btn14 = new Button();
+            btn15 = new Button();
+            btn16 = new Button();
+            gbxMain.SuspendLayout();
+            SuspendLayout();
+            // 
+            // gbxMain
+            // 
+            gbxMain.Controls.Add(btn1);
+            gbxMain.Controls.Add(btn2);
+            gbxMain.Controls.Add(btn3);
+            gbxMain.Controls.Add(btn4);
+            gbxMain.Controls.Add(btn5);
+            gbxMain.Controls.Add(btn6);
+            gbxMain.Controls.Add(btn7);
+            gbxMain.Controls.Add(btn8);
+            gbxMain.Controls.Add(btn9);
+            gbxMain.Controls.Add(btn10);
+            gbxMain.Controls.Add(btn11);
+            gbxMain.Controls.Add(btn12);
+            gbxMain.Controls.Add(btn13);
+            gbxMain.Controls.Add(btn14);
+            gbxMain.Controls.Add(btn15);
+            gbxMain.Controls.Add(btn16);
+            gbxMain.Location = new Point(12, 12);
+            gbxMain.Name = "gbxMain";
+            gbxMain.Size = new Size(387, 372);
+            gbxMain.TabIndex = 0;
+            gbxMain.TabStop = false;
+            // 
+            // btn1
+            // 
+            btn1.Location = new Point(18, 26);
+            btn1.Name = "btn1";
+            btn1.Size = new Size(83, 78);
+            btn1.TabIndex = 15;
+            btn1.UseVisualStyleBackColor = true;
+            btn1.Click += btnButtons_Click;
+            // 
+            // btn2
+            // 
+            btn2.Location = new Point(107, 26);
+            btn2.Name = "btn2";
+            btn2.Size = new Size(83, 78);
+            btn2.TabIndex = 14;
+            btn2.UseVisualStyleBackColor = true;
+            btn2.Click += btnButtons_Click;
+            // 
+            // btn3
+            // 
+            btn3.Location = new Point(196, 26);
+            btn3.Name = "btn3";
+            btn3.Size = new Size(83, 78);
+            btn3.TabIndex = 13;
+            btn3.UseVisualStyleBackColor = true;
+            btn3.Click += btnButtons_Click;
+            // 
+            // btn4
+            // 
+            btn4.Location = new Point(285, 26);
+            btn4.Name = "btn4";
+            btn4.Size = new Size(83, 78);
+            btn4.TabIndex = 12;
+            btn4.UseVisualStyleBackColor = true;
+            btn4.Click += btnButtons_Click;
+            // 
+            // btn5
+            // 
+            btn5.Location = new Point(18, 110);
+            btn5.Name = "btn5";
+            btn5.Size = new Size(83, 78);
+            btn5.TabIndex = 11;
+            btn5.UseVisualStyleBackColor = true;
+            btn5.Click += btnButtons_Click;
+            // 
+            // btn6
+            // 
+            btn6.Location = new Point(107, 110);
+            btn6.Name = "btn6";
+            btn6.Size = new Size(83, 78);
+            btn6.TabIndex = 10;
+            btn6.UseVisualStyleBackColor = true;
+            btn6.Click += btnButtons_Click;
+            // 
+            // btn7
+            // 
+            btn7.Location = new Point(196, 110);
+            btn7.Name = "btn7";
+            btn7.Size = new Size(83, 78);
+            btn7.TabIndex = 9;
+            btn7.UseVisualStyleBackColor = true;
+            btn7.Click += btnButtons_Click;
+            // 
+            // btn8
+            // 
+            btn8.Location = new Point(285, 110);
+            btn8.Name = "btn8";
+            btn8.Size = new Size(83, 78);
+            btn8.TabIndex = 8;
+            btn8.UseVisualStyleBackColor = true;
+            btn8.Click += btnButtons_Click;
+            // 
+            // btn9
+            // 
+            btn9.Location = new Point(18, 194);
+            btn9.Name = "btn9";
+            btn9.Size = new Size(83, 78);
+            btn9.TabIndex = 7;
+            btn9.UseVisualStyleBackColor = true;
+            btn9.Click += btnButtons_Click;
+            // 
+            // btn10
+            // 
+            btn10.Location = new Point(107, 194);
+            btn10.Name = "btn10";
+            btn10.Size = new Size(83, 78);
+            btn10.TabIndex = 6;
+            btn10.UseVisualStyleBackColor = true;
+            btn10.Click += btnButtons_Click;
+            // 
+            // btn11
+            // 
+            btn11.Location = new Point(196, 194);
+            btn11.Name = "btn11";
+            btn11.Size = new Size(83, 78);
+            btn11.TabIndex = 5;
+            btn11.UseVisualStyleBackColor = true;
+            btn11.Click += btnButtons_Click;
+            // 
+            // btn12
+            // 
+            btn12.Location = new Point(285, 194);
+            btn12.Name = "btn12";
+            btn12.Size = new Size(83, 78);
+            btn12.TabIndex = 4;
+            btn12.UseVisualStyleBackColor = true;
+            btn12.Click += btnButtons_Click;
+            // 
+            // btn13
+            // 
+            btn13.Location = new Point(18, 278);
+            btn13.Name = "btn13";
+            btn13.Size = new Size(83, 78);
+            btn13.TabIndex = 3;
+            btn13.UseVisualStyleBackColor = true;
+            btn13.Click += btnButtons_Click;
+            // 
+            // btn14
+            // 
+            btn14.Location = new Point(107, 278);
+            btn14.Name = "btn14";
+            btn14.Size = new Size(83, 78);
+            btn14.TabIndex = 2;
+            btn14.UseVisualStyleBackColor = true;
+            btn14.Click += btnButtons_Click;
+            // 
+            // btn15
+            // 
+            btn15.Location = new Point(196, 278);
+            btn15.Name = "btn15";
+            btn15.Size = new Size(83, 78);
+            btn15.TabIndex = 1;
+            btn15.UseVisualStyleBackColor = true;
+            btn15.Click += btnButtons_Click;
+            // 
+            // btn16
+            // 
+            btn16.Location = new Point(285, 278);
+            btn16.Name = "btn16";
+            btn16.Size = new Size(83, 78);
+            btn16.TabIndex = 0;
+            btn16.UseVisualStyleBackColor = true;
+            btn16.Click += btnButtons_Click;
+            // 
+            // frmGame
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(414, 403);
+            Controls.Add(gbxMain);
+            FormBorderStyle = FormBorderStyle.FixedSingle;
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "frmGame";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Puzzle";
+            gbxMain.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private GroupBox gbxMain;
+        private Button btn2;
+        private Button btn3;
+        private Button btn4;
+        private Button btn5;
+        private Button btn6;
+        private Button btn7;
+        private Button btn1;
+        private Button btn8;
+        private Button btn9;
+        private Button btn10;
+        private Button btn11;
+        private Button btn12;
+        private Button btn13;
+        private Button btn14;
+        private Button btn15;
+        private Button btn16;
+    }
+}
---
+++ puzzle/Form1.cs
@@ -0,0 +1,112 @@
+using System.Linq.Expressions;
+using System.Runtime.InteropServices;
+
+namespace puzzle
+{
+    public partial class frmGame : Form
+    {
+
+        public frmGame()
+        {
+            InitializeComponent();
+            Shuffle(random);
+         
+            for(int i = 0; i < 4; i++)
+            {
+                for(int j = 0; j < 4; j++)
+                {
+                    if(index < gbxMain.Controls.Count)
+                    {
+                        buttons[i,j] = (Button) gbxMain.Controls[index];
+                        index++;
+                    }
+                }
+            }
+            for (int i = 0; i < gbxMain.Controls.Count; i++)
+            {
+                if (gbxMain.Controls[i].Name != "btn16")
+                {
+                    gbxMain.Controls[i].Text = $"{numbers[i]}";
+                }
+            }
+        }
+
+        #region variables
+        Button[,] buttons = new Button[4, 4];
+        int index = 0;
+        Random random = new Random();
+        List<int> numbers = new List<int>([1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]);
+        #endregion variables
+
+        #region events
+        private void btnButtons_Click(object sender, EventArgs e)
+        {
+            ChangeButtons(sender);
+        }
+        #endregion events
+
+        #region methods
+
+        void ChangeButtons(object sender)
+        {
+            Button button = (Button)sender;
+            for(int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (button == buttons[i, j])
+                    {
+                        try
+                        {
+                            if (buttons[i, j + 1].Text == "")
+                            {
+                                buttons[i, j + 1].Text = button.Text;
+                                button.Text = "";
+                            }
+                        }
+                        catch { }
+                        try
+                        {
+                            if (buttons[i, j - 1].Text == "")
+                            {
+                                buttons[i, j - 1].Text = button.Text;
+                                button.Text = "";
+                            }
+                        }
+                        catch { }
+                        try
+                        {
+                            if (buttons[i + 1, j].Text == "")
+                            {
+                                buttons[i + 1, j].Text = button.Text;
+                                button. Text = "";
+                            }
+                        }
+                        catch { }
+                        try
+                        {
+                            if (buttons[i - 1, j].Text == "")
+                            {
+                                buttons[i - 1, j].Text = button.Text;
+                                button.Text = "";
+                            }
+                        }
+                        catch {}
+                    }
+                }
+            }
+        }
+        void CheckIfWin()
+        {
+
+        }
+
+        void  Shuffle(Random random)
+        {
+            numbers = numbers.OrderBy(x => random.Next(1,16)).ToList();
+            
+        }
+
+#endregion methods
+    }
+}
